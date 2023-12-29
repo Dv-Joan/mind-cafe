cb1f5a7e1434573847192497368232d56ff55165
Joan <joan300501@gmail.com>
2023-12-29 07:59:38 -0500

    Add tsconfigPaths to app.json and update import paths

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ import {
   Drawer,
 } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useUserContext } from "../../context/UserContext";
+import { useUserContext } from "@/context/UserContext";
 const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 const User = {
