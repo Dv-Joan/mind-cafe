44e649760e1f30dd025b9cff9228190a1f5177ef
Brayan <joan300501@gmail.com>
2024-01-03 07:10:19 -0500

    Sign Up screen and validation with zod and rhf

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,8 +1,5 @@
+import { Stack } from "expo-router";
 import React from "react";
-import { Link, Stack, router } from "expo-router";
-import { Button } from "react-native-paper";
-import { Ionicons } from "@expo/vector-icons";
-import { TouchableOpacity } from "react-native";
 
 export default function AuthLayout() {
   return (
@@ -11,26 +8,8 @@ export default function AuthLayout() {
         name="index"
         options={{
           presentation: "card",
-          title: "Sign Up",
-          headerRight: () => {
-            return (
-              <Button
-                onPress={() => router.push({ pathname: "/(auth)/login" })}
-                mode="text"
-              >
-                Login
-              </Button>
-            );
-          },
-          headerLeft: () => {
-            return (
-              <Link href="/(tabs)/" asChild>
-                <TouchableOpacity className="flex flex-row -ml-3 items-center">
-                  <Ionicons name="ios-chevron-back" size={24} color="indigo" />
-                </TouchableOpacity>
-              </Link>
-            );
-          },
+          title: "",
+          headerShown: false,
         }}
       />
       <Stack.Screen
--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,161 @@
-import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React from "react";
+import {
+  Keyboard,
+  Linking,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button, HelperText, TextInput } from "react-native-paper";
+
+const schema = z.object({
+  username: z.string().min(3).max(20),
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
+});
 
 export default function Login() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <View className="p-4">
-      <Text className="text-xl font-bold">Sign Up</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <ScrollView>
+        <View className="p-4 bg-[#1E1C2A] pt-16 space-y-6 h-screen relative ">
+          <View className="absolute -top-10 left-32 right-0 h-1/5 bg-[#00B661] rounded-2xl shadow-lg shadow-black/50 w-1/5 rotate-[30deg] " />
+          <View className="bg-[#1C2F32] rounded-full p-3 w-14 ">
+            <Ionicons name="home-outline" size={30} color="#00B661" bg />
+          </View>
+          <View className=" space-y-2 ">
+            <Text className="text-white text-2xl font-bold">
+              Let's log you in
+            </Text>
+            <Text className="text-[#9897A0] text-sm">
+              Welcome back, you've been missed!
+            </Text>
+          </View>
+          <View className="flex flex-row  gap-2 pr-4">
+            <Button
+              className="rounded-xl w-1/2 active:opacity-70 "
+              mode="contained"
+              buttonColor="#EC4033"
+              icon="google-plus"
+              onPress={() => Linking.openURL("https://google.com")}
+            >
+              Google
+            </Button>
+
+            <Button
+              className="rounded-xl w-1/2 active:opacity-70"
+              mode="contained"
+              buttonColor="#1876F2"
+              icon="facebook"
+              onPress={() => Linking.openURL("https://facebook.com")}
+            >
+              Facebook
+            </Button>
+          </View>
+
+          <View className="space-y-6">
+            <View>
+              <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    className="rounded-xl  bg-[#24273A]"
+                    textColor="white"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    placeholder="Username"
+                  />
+                )}
+                name="username"
+                defaultValue=""
+              />
+              {errors.username && (
+                <HelperText type="error">This is required.</HelperText>
+              )}
+            </View>
+            <View>
+              <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    className="rounded-xl bg-[#24273A] "
+                    onBlur={onBlur}
+                    textColor="white"
+                    onChangeText={onChange}
+                    value={value}
+                    placeholder="Email Adress"
+                  />
+                )}
+                name="email"
+                defaultValue=""
+              />
+              {errors.email && (
+                <HelperText type="error">This is required.</HelperText>
+              )}
+            </View>
+            <View>
+              <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    className="rounded-xl  bg-[#24273A] "
+                    onBlur={onBlur}
+                    textColor="white"
+                    secureTextEntry={!showPassword}
+                    right={
+                      <TextInput.Icon
+                        onPress={() => setShowPassword(!showPassword)}
+                        icon={showPassword ? "eye" : "eye-off"}
+                      />
+                    }
+                    onChangeText={onChange}
+                    value={value}
+                    placeholder="Password"
+                  />
+                )}
+                name="password"
+                defaultValue=""
+              />
+              {errors.password && (
+                <HelperText type="error">This is required.</HelperText>
+              )}
+            </View>
+          </View>
+          <Button
+            className="rounded-xl active:opacity-70 py-2"
+            mode="contained"
+            buttonColor="#00B661"
+            onPress={handleSubmit(onSubmit)}
+          >
+            Sign Up
+          </Button>
+          <View className="flex flex-row justify-center">
+            <Text className=" text-[#9897A0]">Don't have an account?</Text>
+            <TouchableOpacity onPress={() => router.push("/(auth)/login")}>
+              <Text className=" text-[#1876F2] font-semibold "> Sign In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </TouchableWithoutFeedback>
   );
 }
